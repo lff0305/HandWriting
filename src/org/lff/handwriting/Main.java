5818063a54ec14ef781c073171c84932646db919
FeifeiLiu <lff0305@gmail.com>
2017-11-21 10:17:13 +0800

    No issue. Center dialog.

--- src/org/lff/handwriting/Main.java
+++ src/org/lff/handwriting/Main.java
@@ -1,5 +1,7 @@
 package org.lff.handwriting;
 
+import java.awt.*;
+
 /**
  * @author Feifei Liu
  * @datetime 2017-11-15  12:04
@@ -8,7 +10,9 @@ public class Main {
     public static void main(String[] argu) {
         MainDialog dlg = new MainDialog();
         dlg.setSize(800, 600);
-        dlg.setLocation(300, 200);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        dlg.setLocation(dim.width / 2 - dlg.getSize().width / 2, dim.height / 2 - dlg.getSize().height / 2);
         dlg.setVisible(true);
     }
 }
